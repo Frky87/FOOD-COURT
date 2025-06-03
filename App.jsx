59f47b818b55370f6fee70e4f3b54f578fb4e525
Frky87 <153166024+Frky87@users.noreply.github.com>
2025-06-04 01:35:01 +0700

    Final

--- App.jsx
+++ App.jsx
@@ -1,10 +1,38 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './src/navigation/Router';
 import { CartProvider } from './src/context/CartContext';
 import { ProductProvider } from './src/context/ProductContext';
+import messaging from '@react-native-firebase/messaging';
+import { PermissionsAndroid } from 'react-native';
+
+async function registerAppWithFCM() {
+  // Mendaftar perangkat untuk menerima remote notifications
+  await messaging().registerDeviceForRemoteMessages();
+
+  // Mendapatkan token perangkat yang diperlukan untuk pengiriman pesan
+  const token = await messaging().getToken();
+  console.log('FCM token:', token); // Log token untuk digunakan di server atau Firebase
+}
+
+// Meminta izin untuk menerima notifikasi
+async function requestPermissions() {
+  const granted = await PermissionsAndroid.request(
+    PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
+  );
+  if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    console.log('Notification permission granted');
+    registerAppWithFCM(); // Daftarkan aplikasi setelah izin diberikan
+  } else {
+    console.log('Notification permission denied');
+  }
+}
 
 export default function App() {
+  useEffect(() => {
+    requestPermissions(); // Meminta izin saat aplikasi dimulai
+  }, []);
+
   return (
     <ProductProvider>
       <CartProvider>
